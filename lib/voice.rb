5a4a0a62108fde6215a3cbd9d06078076667c884
Ole Friis Østergaard <olefriis@gmail.com>
2022-05-26 15:02:18 +0200

    Only do stuff when gate changes

--- lib/voice.rb
+++ lib/voice.rb
@@ -20,26 +20,16 @@ class Voice
     @attack_decay = @sustain_release = 0
     @control_register = 0
     @playing = false
-
-    @old_frequency = 0
   end
 
   def finish_frame(osc_client)
-    if gate
-      if frequency > 0 && (!@playing || @old_frequency != frequency)
-        osc_client.send OSC::Message.new('/trigger', @channel, waveform, midi_note, attack, decay, sustain_level)
-
-        @playing = true
-      end
-    elsif @playing
-      osc_client.send OSC::Message.new('/trigger', @channel, 0, midi_note, release, 0, 0)
-
-      @playing = false
-    elsif @old_frequency != frequency
+    if gate && !@playing
+      osc_client.send OSC::Message.new('/trigger', @channel, waveform, midi_note, 0, 0, 1)
+      @playing = true
+    elsif !gate && @playing
       osc_client.send OSC::Message.new('/trigger', @channel, 0, midi_note, 0, 0, 0)
+      @playing = false
     end
-
-    @old_frequency = frequency
   end
 
   private
@@ -66,64 +56,5 @@ class Voice
     return 4 if @control_register & 128 != 0 # Noise
     STDERR.puts "Unknown waveform: #{@control_register}"
     return :noise
-  end
-
-  def sustain_level
-    (@sustain_release >> 4) / 15.0
-  end
-
-  def decay
-    # Approximated... should be multiplied by 1.000.000 / clock
-    convert_decay_or_release(@attack_decay & 0xF)
-  end
-
-  def release
-    # Approximated... should be multiplied by 1.000.000 / clock
-    convert_decay_or_release(@sustain_release & 0xF)
-  end
-
-  def attack
-    # Approximated... should be multiplied by 1.000.000 / clock
-
-    case @attack_decay >> 4
-    when 0 then 0.002
-    when 1 then 0.008
-    when 2 then 0.016
-    when 3 then 0.024
-    when 4 then 0.038
-    when 5 then 0.056
-    when 6 then 0.068
-    when 7 then 0.08
-    when 8 then 0.1
-    when 9 then 0.25
-    when 10 then 0.5
-    when 11 then 0.8
-    when 12 then 1
-    when 13 then 3
-    when 14 then 5
-    when 15 then 8
-    else raise "Unknown value: #{attack}"
-    end
-  end
-
-  def convert_decay_or_release(decay_or_release)
-    case decay_or_release
-    when 0 then 0.006
-    when 1 then 0.024
-    when 2 then 0.048
-    when 3 then 0.072
-    when 4 then 0.114
-    when 5 then 0.168
-    when 6 then 0.204
-    when 7 then 0.240
-    when 8 then 0.3
-    when 9 then 0.75
-    when 10 then 1.5
-    when 11 then 2.4
-    when 12 then 3
-    when 13 then 9
-    when 14 then 15
-    when 15 then 24
-    end
   end
 end
